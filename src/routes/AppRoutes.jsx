8f8e0fd2bbb6f98b4e61d8899adafa5cf7f613c8
Saurav Pant <sauravpant777@gmail.com>
2025-04-13 01:51:19 +0545

    Update routes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,6 +2,8 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Login from "../pages/auth/Login";
 import SignUp from "../pages/auth/SignUp";
 import Home from "../pages/Home";
+import Error404 from "../pages/Error404";
+import Products from "../components/common/Products";
 import { MainLayout } from "../components/layout/MainLayout";
 const AppRoutes = () => {
   return (
@@ -9,9 +11,11 @@ const AppRoutes = () => {
       <Routes>
         <Route element={<MainLayout />}>
           <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products/>}></Route>
         </Route>
         <Route path="/auth/login" element={<Login />}></Route>
         <Route path="/auth/signup" element={<SignUp />}></Route>
+        <Route path="/404" element={<Error404 />}></Route>
       </Routes>
     </BrowserRouter>
   );
