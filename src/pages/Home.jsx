6a3cd2731fb8520f73dd76f93b366d7f29725c70
Saurav Pant <sauravpant777@gmail.com>
2025-04-12 21:27:38 +0545

    Add UI

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Banner from '../components/home/Banner'
+import FeaturedProducts from '../components/home/FeaturedProducts'
+const Home = () => {
+  return (
+    <>
+    <Banner/>
+    <FeaturedProducts/>
+    </>
+  )
+}
+
+export default Home
