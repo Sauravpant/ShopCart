d4576f0d9a32d172915fb99f12950f63aa07c574
Saurav Pant <sauravpant777@gmail.com>
2025-04-12 21:29:00 +0545

    Minor changes

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -10,7 +10,7 @@ import {
 
 const Footer = () => {        
     return (
-        <section className="py-10 bg-gray-100 sm:pt-16 lg:pt-24">
+        <section className="py-10 bg-gray-300 sm:pt-16 lg:pt-24">
     <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
         <div className="grid grid-cols-2 md:col-span-3 lg:grid-cols-6 gap-y-16 gap-x-12">
             <div className="col-span-2 md:col-span-3 lg:col-span-2 lg:pr-8">
@@ -88,7 +88,7 @@ const Footer = () => {
             </div>
 
             <div className="col-span-2 md:col-span-1 lg:col-span-2 lg:pl-8">
-                <p className="text-sm font-semibold tracking-widest text-gray-400 uppercase">Subscribe to newsletter</p>
+                <p className="text-sm font-semibold tracking-widest text-gray-400 uppercase">Mail Us</p>
 
                 <form action="#" method="POST" className="mt-6">
                     <div>
@@ -96,7 +96,7 @@ const Footer = () => {
                         <input type="email" name="email" id="email" placeholder="Enter your email" className="block w-full p-4 text-black placeholder-gray-500 transition-all duration-200 bg-white border border-gray-200 rounded-md focus:outline-none focus:border-blue-600 caret-blue-600" />
                     </div>
 
-                    <button type="submit" className="inline-flex items-center justify-center px-6 py-4 mt-3 font-semibold text-white transition-all duration-200 bg-blue-600 rounded-md hover:bg-blue-700 focus:bg-blue-700">Subscribe</button>
+                    <button type="submit" className="inline-flex items-center justify-center px-6 py-4 mt-3 font-semibold text-white transition-all duration-200 bg-blue-600 rounded-md hover:bg-blue-700 focus:bg-blue-700">Submit</button>
                 </form>
             </div>
         </div>
