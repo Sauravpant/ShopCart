cebb8c099389ae2995aab082de8e8e7cc2371d59
Saurav Pant <sauravpant777@gmail.com>
2025-04-15 10:54:06 +0545

    Minor Changes

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice.js";
-import cartReducer from "../features/cart/cartSlice.js"
-
-export const store =configureStore({
-  reducer:{
-    auth:authReducer,
-    cart:cartReducer,
+import cartReducer from "../features/cart/cartSlice.js";
+import productReducer from "../features/filters/productSlice.js"
+export const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    cart: cartReducer,
+    product: productReducer,
   },
 });
